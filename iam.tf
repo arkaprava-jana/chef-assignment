47e1dd335937f6ee81e20305ca685d723a984df6
cloudpunk <arkaprava.j@gmail.com>
2021-10-07 21:55:41 +0530

    clean up

--- iam.tf
+++ iam.tf
@@ -130,3 +130,24 @@ resource "aws_iam_policy_attachment" "sftpMonitoringRolePolicyAttach" {
 }
 
 
+resource "aws_iam_role" "lambdaIAMRole" {
+  name = "lambdaIAMRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "lambdaIAMRole"
+  }
+}
